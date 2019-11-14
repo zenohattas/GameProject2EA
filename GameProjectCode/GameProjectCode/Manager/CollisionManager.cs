489a3925bcc86d90c161dbadd9771e8267149d48
Zeno <s102215@ap.be>
2019-11-14 10:10:12 +0100

    Updated the collisionManager

--- GameProjectCode/GameProjectCode/Manager/CollisionManager.cs
+++ GameProjectCode/GameProjectCode/Manager/CollisionManager.cs
@@ -1,5 +1,6 @@
 ﻿using GameProjectCode.Models;
 using GameProjectCode.Objects;
+using Microsoft.Xna.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,11 +23,78 @@ namespace GameProjectCode.Manager
             }
             return collidableSprites;
         }
+
+        /// <summary>
+        /// Detects overlap between ICollidables.
+        /// </summary>
+        /// <param name="o1"></param>
+        /// <param name="o2"></param>
+        /// <returns></returns>
         public bool DetectCollision(ICollidable o1, ICollidable o2)
         {
             if (o1.CollisionRectangle.Intersects(o2.CollisionRectangle))
                 return true;
             return false;
+        }
+
+        /// <summary>
+        /// Detects and executes all collision functions of the objects that collide with eachother.
+        /// </summary>
+        /// <param name="sprites"></param>
+        public void DetectCollisions(List<GameObject> sprites)
+        {
+            List<ICollidable> collidables = GetCollidableList(sprites);
+            for (int i = 0; i < collidables.Count; i++)
+            {
+                if (collidables[i] is MoveableGameObject)
+                {
+                    for (int j = 0; j < collidables.Count; j++)
+                    {
+                        if (i != j)
+                        {
+                            if (DetectCollision(collidables[i], collidables[j]))
+                            {
+                                collidables[i].Collide(collidables[j]);
+                                if (collidables[j] is MoveableGameObject)
+                                    collidables[j].Collide(collidables[i]);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// Calculates the side of the collision. Returns 0 if a side doesn't collide.
+        /// </summary>
+        /// <param name="o1"></param>
+        /// <param name="o2"></param>
+        /// <returns></returns>
+        public Rectangle GetCollisionEdge(ICollidable o1, ICollidable o2)
+        {
+            Rectangle collisionSide = new Rectangle();
+
+            //Left side collides of o2
+            if (o1.Position.X + o1.Dimenions.X > o2.Position.X && o1.Position.X < o2.Position.X)
+            {
+                collisionSide.X--;    
+            }
+            //Right side collides of o2
+            if (o2.Position.X + o2.Dimenions.X > o1.Position.X && o2.Position.X < o1.Position.X)
+            {
+                collisionSide.X++;
+            }
+            //Bottom side collides of o2
+            if (o2.Position.Y + o2.Dimenions.Y > o1.Position.Y && o2.Position.Y < o1.Position.Y)
+            {
+                collisionSide.Y--;
+            }
+            //Top side collides of o2
+            if (o2.Position.Y < o1.Position.Y + o1.Dimenions.Y && o2.Position.Y > o1.Position.Y)
+            {
+                collisionSide.Y++;
+            }
+
+            return collisionSide;
         }
     }
 }
