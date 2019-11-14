80bdfc384027ac630177f9d875eaec8ba27f8054
Zeno <s102215@ap.be>
2019-11-14 10:11:38 +0100

    Updated GameOpject

--- GameProjectCode/GameProjectCode/Objects/GameObject.cs
+++ GameProjectCode/GameProjectCode/Objects/GameObject.cs
@@ -12,10 +12,26 @@ namespace GameProjectCode.Objects
 {
     abstract class GameObject
     {
+        /// <summary>
+        /// Create gameobject with animation being the first value of the animations library
+        /// </summary>
+        /// <param name="animations"></param>
         public GameObject(Dictionary<string, Animation> animations)
         {
             _animations = animations;
+
             _animationManager = new AnimationManager(_animations.First().Value);
+        }
+        /// <summary>
+        /// Create a gameobject with animation initialised on a specific sprite
+        /// </summary>
+        /// <param name="animations"></param>
+        /// <param name="animation"></param>
+        public GameObject(Dictionary<string, Animation> animations, Animation animation)
+        {
+            _animations = animations;
+
+            _animationManager = new AnimationManager(animation);
         }
 
         protected AnimationManager _animationManager;
