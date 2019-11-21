b1339eff4a9a3d8e9c66642c105cae36c8e79eef
Zeno <s102215@ap.be>
2019-11-21 14:44:10 +0100

    Added MenuEllement model

---
+++ GameProjectCode/GameProjectCode/Models/MenuElement.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProjectCode.Models
+{
+    class MenuElement
+    {
+        public string Text { get; set; }
+        public Vector2 Position { get; set; }
+        public Color Color { get; set; }
+        public MenuElement(string text, Vector2 position)
+        {
+            Text = text;
+            Position = position;
+        }
+    }
+}
